893320a36a3d56c59bc9cc54537e4453cc89bbcd
Alex Newbegin <alex.newbegin95@gmail.com>
2023-09-19 21:48:16 -0400

    feat: built out sample size formula; updated baseline dataset to introduce more variance

--- ab_test.py
+++ ab_test.py
@@ -1,20 +1,22 @@
 # %%
 import pandas as pd
 import numpy as np
-import math
+import statsmodels.stats.power as smp
 
+# %%
 baseline_csv = 'baseline_clickthrough_rate.csv'
 
 df = pd.read_csv(baseline_csv)
 # print(df.head())
 
-x_bar = df['Clickthrough_Rate'].mean()
-x_vals = df['Clickthrough_Rate'].tolist()
+# %%
+x_vals = [x * 100 for x in df['Clickthrough_Rate'].tolist()]
+x_bar = np.mean(x_vals)
 k = len(x_vals)
 
-# print(x_bar)
-# print(x_vals)
-# print(k)
+print(x_bar)
+print(x_vals)
+print(k)
 
 sum = 0
 
@@ -23,4 +25,13 @@ for x_i in x_vals:
 
 var = (sum / (k - 1)) * 2
 print(var)
+# %%
+delta_squared = (.05 - x_bar) ** 2
+print(delta_squared)
+# %%
+z_crit_alpha = 1.96
+z_crit_beta = .84
+
+sample_size = ((z_crit_alpha + z_crit_beta) ** 2)
+print(sample_size)
 # %%
